20484648a5f6e2fb734201af9d75e542b23410f5
Neyzimho <flasney.medeiros@outlook.com>
2026-01-15 14:09:50 -0300

    First commit

---
+++ src/test/java/com/helios/helios/observability/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.helios.helios.observability;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
